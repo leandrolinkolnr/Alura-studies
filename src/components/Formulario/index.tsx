85fa921509e459de78c1f6f08eff098bae09c968
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2023-02-20 11:18:37 -0300

    Usando Props no botao

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -21,7 +21,7 @@ class Formulario extends React.Component {
                     <input type="time" step="1" name="tempo" id="tempo" 
                     min="00:00:00" max="01:30:00" required />
                 </div>
-                <Botao/>
+                <Botao texto="Adicionar" />
             </form>
         )
     }
