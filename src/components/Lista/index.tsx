d5c2c34a9aae8e3f38c16c261357fe01a69a66ee
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2023-02-20 12:09:02 -0300

    Refatorando a lista usando props

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Item } from "./Item";
 import style from './Lista.module.scss'
 
 export function Lista(){
@@ -15,13 +16,17 @@ export function Lista(){
     return(
         <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
-            <ul>
+            <ul> {/*
                 {tarefas.map((item, index) => (
-                    <li key={index} className={style.item} > 
-                        <h3>{item.tarefa}</h3>
-                        <span>{item.tempo}</span>
-                    </li>
-              ))}
+                  <Item key={index}
+                  tarefa={item.tarefa}
+                  tempo={item.tempo} />
+                ))} */}
+
+                {tarefas.map((item, index) => (
+                  <Item  key={index}
+                  {...item}   />
+                ))}     
             </ul>
         </aside>
     )
