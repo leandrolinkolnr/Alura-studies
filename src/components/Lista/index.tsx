15b10cd925ad6f98aafca0528e1b0aabdb537d69
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2023-02-21 16:33:47 -0300

    UseState Tarefas
    Adicionando tarefas usando useState

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,33 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Item } from "./Item";
 import style from './Lista.module.scss'
 
 export function Lista(){
 
-    const tarefas = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-      }, {
-        tarefa: 'Javascript',
-        tempo: '01:00:00'
-      }];
+    const [tarefas, setTarefas] = useState([{
+      tarefa: 'React',
+      tempo: '02:00:00'
+    }, {
+      tarefa: 'Javascript',
+      tempo: '01:00:00'
+    }]);
 
 
     return(
         <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
-            <ul> {/*
+            <h2 onClick={() => {
+              setTarefas([...tarefas, {tarefa: "Estudar estado", tempo: "05:00:00"}]);    {/* Essa expressao quer dizer que tarefas é igual a ele mesmo, mais o arqumento da direita*/}
+            }}     >Estudos do dia</h2>
+            <ul> 
+                {tarefas.map((item, index) => (
+                  <Item  key={index}
+                  {...item}   />
+                ))} 
+
+                {/*
                 {tarefas.map((item, index) => (
                   <Item key={index}
                   tarefa={item.tarefa}
                   tempo={item.tempo} />
                 ))} */}
 
-                {tarefas.map((item, index) => (
-                  <Item  key={index}
-                  {...item}   />
-                ))}     
             </ul>
+
+            
         </aside>
     )
 }
