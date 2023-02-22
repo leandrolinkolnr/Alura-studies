3fdfad97f852b17912aad78ffad91c11f8d19fd3
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2023-02-22 13:37:49 -0300

    State Tarefa

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -3,23 +3,37 @@ import Botao from '../Botao';
 import style from './Formulario.module.scss'
 
 class Formulario extends React.Component {
+    state = {
+        tarefa: "",
+        tempo: "00:00"
+    }
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
+        evento.preventDefault();
+        console.log('state:', this.state)  // Impede que a pagina seja atualizada automaticamente
+
+    }
+
     render(){
         return(
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)} >
                 <div className={style.inputContainer}>
                     <label htmlFor='tarefa'>
                         Adicione uma tarefa
                     </label>
-                    <input type="text" name="tarefa" id="tarefa" 
-                    placeholder='O que você quer estudar?' required/>
+                    <input type="text" name="tarefa" 
+                    value={this.state.tarefa} onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
+                    id="tarefa" placeholder='O que você quer estudar?' required/>
                 </div>
 
                 <div className={style.inputContainer}>
                     <label htmlFor="tempo">
                         Tempo
                     </label>
-                    <input type="time" step="1" name="tempo" id="tempo" 
-                    min="00:00:00" max="01:30:00" required />
+                    <input type="time" step="1" name="tempo" 
+                    value={this.state.tempo} 
+                    onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
+                    id="tempo" min="00:00:00" max="01:30:00" required />
                 </div>
                 <Botao texto="Adicionar" />
             </form>
