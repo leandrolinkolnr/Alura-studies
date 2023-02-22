1dedeed87b1e65eb5484f8dd5d5a43fa42fb8227
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2023-02-22 16:49:33 -0300

    Atualizar Tarefas

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -2,21 +2,23 @@ import React from 'react';
 import Botao from '../Botao';
 import style from './Formulario.module.scss'
 import { Itarefa } from '../../types/tarefa';
+import { v4 as uuidv4 } from 'uuid';
 
 class Formulario extends React.Component<{setTarefas:
 React.Dispatch<React.SetStateAction<Itarefa[]>>}> 
-
     {
     state = {
         tarefa: "",
         tempo: "00:00"
     }
 
-
     adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
-        evento.preventDefault(); // Impede que a pagina seja atualizada automaticamente
-        this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, {...this.state}])
-
+        evento.preventDefault();        // Impede que a pagina seja atualizada automaticamente
+        this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, {...this.state, selecionado: false, completado: false, id: uuidv4()}])
+        this.setState({
+            tarefa: "",
+            tempo: "00:00"
+        })
     }
 
     render(){
--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,8 +1,8 @@
 import style from '../Lista.module.scss'
+import { Itarefa } from '../../../types/tarefa';
 
-export function Item({tarefa, tempo }: {tarefa: string, tempo: string}){
+export function Item({tarefa, tempo, selecionado, completado, id }: Itarefa){
    
-
     return(
         <li  className={style.item} > 
             <h3>{tarefa}</h3>
